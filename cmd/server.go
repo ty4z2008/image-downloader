5e099450a426201151426221f29ea994631cd9f6
ty4z2008 <ty4z2008@gmail.com>
2020-04-21 18:30:00 +0800

    init project

---
+++ cmd/server.go
@@ -0,0 +1,11 @@
+/*
+* @Author: Jeffery
+* @Date:   2020-04-21 18:29:00
+* @Last Modified by:   Jeffery
+* @Last Modified time: 2020-04-21 18:29:38
+ */
+package main
+
+func main() {
+	println("start")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ty4z2008/doubandownloder
+
+go 1.13
